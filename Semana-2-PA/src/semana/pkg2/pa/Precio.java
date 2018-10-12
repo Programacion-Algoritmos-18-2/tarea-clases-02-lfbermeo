c6e8d4b58ea9a50a7a006f0493e8b4c6e2fb2124
Luisa Bermeo <ferbermeo04@gmail.com>
2018-10-12 10:22:23 -0500

    Agregando tarea

---
+++ Semana-2-PA/src/semana/pkg2/pa/Precio.java
@@ -0,0 +1,15 @@
+package semana.pkg2.pa;
+
+public class Precio {
+
+    double euros;
+
+    public double obtener() {
+        return euros;
+    }
+
+    public void agregar(double x) {
+
+    }
+
+}
---
+++ Semana-2-PA/src/semana/pkg2/pa/Principal.java
@@ -0,0 +1,10 @@
+package semana.pkg2.pa;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Precio p;
+        p = new Precio();
+        System.out.println(p.euros);
+    }
+}
